5dcd411ccef5faf4d736dd98b3f58b4567946fbb
KriStar88 <kristina_arapova@mail.ru>
2022-05-25 21:32:45 +0300

    add constructors

--- Tetris/Program.cs
+++ Tetris/Program.cs
@@ -9,18 +9,18 @@ Console.SetBufferSize(40, 30);
 
 
 
-Point p1 = new Point(2, 3, '*');
+
+
+Point p1 = new Point(2,3, '*');
 
 p1.Draw();
 
 Point p2 = new Point()
 {
-    x = 4;
-    y = 5;
-    c = '#';
-
-}
-
+    x = 4,
+    y = 5,
+    c = '#'
+};
 p2.Draw();
 
 Console.ReadLine();
