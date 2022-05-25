0b36edb2e8348b6afce1a1227c5d7e7cd4a7c811
KriStar88 <kristina_arapova@mail.ru>
2022-05-25 21:38:48 +0300

    add Collections

---
+++ Collection/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Tetris;
+
+int[] nums1 = new int[5];
+nums1[0] = 1;
+nums1[1] = 2;
+//for (int i = 0; i < nums1.Length; i++) 
+//{
+//    Console.WriteLine(nums1[i]);
+//}
+foreach (int i in nums1)
+{
+    Console.WriteLine(i);
+}
+
+Point[] points = new Point[3];
+points[0] = new Point(1, 2, '*');
+points[1] = new Point(3, 4, '#');
+points[2] = new Point(5, 6, '*');
+foreach (Point p in points)
+{
+    p.Draw();
+}
+
+Console.ReadLine();
--- Tetris/Point.cs
+++ Tetris/Point.cs
@@ -6,29 +6,26 @@ using System.Threading.Tasks;
 
 namespace Tetris
 {
-     class Point
+    public class Point
     {
        public int x;
        public int y;
        public char c;
+       
        public void Draw()
         {
             Console.SetCursorPosition(x, y);
             Console.Write(c);
         }
 
-        public Point(int x, int y, char sym)
-
+        public Point(int a, int b, char sym)
         {
             x = a;
             y = b;
             c = sym;
         }
 
-        public Point()
-        { 
-        }
+        public Point() { }
 
-        
     }
 }
